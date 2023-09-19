e013841bdd96dc92e3c6e24725cabba58f5c210b
Kendall Boesch <kboesch@smu.edu>
2023-09-19 15:42:42 -0500

    Created Preprocessing.py to make preprocessing modular

---
+++ Preprocessing.py
@@ -0,0 +1,81 @@
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_path = "/Users/kendallboesch/Desktop/CS5351-SeniorDesign/TestCQ/testImg1.JPG"
+output_path = "/Users/kendallboesch/Desktop/CS5351-SeniorDesign/TestCQ/afterProcessing.jpg"
+
+face_x, face_y, face_w, face_h = 0
+
+def locate_face(image_path) :
+    # image read 
+    img = cv2.imread(image_path)
+    
+    # Convert to gray scale 
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # load pretrained Haar Cascade Classifier 
+    face_classifier = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    
+    # perform facial detection 
+    face = face_classifier.detectMultiScale(
+        img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40,40)
+    )
+    
+    # draw bounding box 
+    for(x, y, w, h) in face : 
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        
+        # Set coordiantes for face location 
+        face_x = x
+        face_y = y
+        face_w = w
+        face_h = h
+        
+    # Print facial dimensions
+    print(f"Face dimensions: \n\tX start: {face_x} \n\tY start: {face_y} \n\tHeight: {face_height} \n\tWidth: {face_width}") 
+
+def crop_to_face(img_path_in, img_path_out) :
+    # image read 
+    img = cv2.imread(img_path_in)
+    
+    # Calculate dimensions 
+    x1 = face_x 
+    x2 = face_x + face_w
+    y1 = face_y 
+    y2 = face_y + face_h
+    
+    # crop image 
+    cropped_image = img[y1:y2, x1:x2]
+    
+    # save cropped image 
+    cv2.imwrite(img_path_out, cropped_image)
+    
+    # return path to cropped image 
+    
+    return img_path_out 
+
+def resize_image(img_path_in, img_path_out, scale_factor) :
+    # image read 
+    img = cv2.imread(img_path_in)
+    
+    # resize dimensions 
+    new_height = int(face_h * scale_factor) 
+    new_width = int(face_w * scale_factor)
+    
+    # resize image 
+    resized_image = cv2.resize(img, (new_width, new_height))
+    
+    # saved resized image 
+    cv2.imwrite(img_path_out, resized_image)
+
+    # return path to cropped image 
+    return img_path_out 
+    
+    
+    
+     
+    
+        
--- VideoProcessing.py
+++ VideoProcessing.py
@@ -1,35 +1,84 @@
 import csv 
 import cv2 
+import numpy as np
+from pytube import YouTube
 
-def process_video(url, t_start, t_end) : 
+
+
+output_path = "/Users/kendallboesch/Desktop/CS5351-SeniorDesign/TestCQ/availableVideos.csv"
+accessed = 0 
+processed = 0
+
+with open(output_path, 'w') as out :
+    out.write("link,start,end,video,utterance,arousal,valence,EmotionMaxVote")
+
+
+def process_video(url, t_start, t_end, video, utterance, arousal, valence, emotionMaxVote, validCount, numProcessed) : 
     
     #initialize video capture 
     vid = cv2.VideoCapture(url)
+    numProcessed += 1
+    
+    if numProcessed % 10 == 0 : 
+        print(f"\t\t\t\t NUM PROCESSED: {numProcessed}")
     
     #ensure video opened 
     if not vid.isOpened(): 
-        print("Error")
+        print("Error") 
     else :
         print("OPENED")
+        validCount +=1
+        entry = url + ',' + str(t_start) + ',' + str(t_end) + ',' + video + ',' + utterance + ',' + str(arousal)+','+str(valence) + ',' + str(emotionMaxVote) + '\n'
+        with open(output_path, 'w') as out :
+            out.write(entry)
+            
+
+def download_youtube(youtube_url, num) :
+    
+    #create youtube object
+    yt = YouTube(youtube_url)
+    
+   
+    
+    # download the video 
+    try :
+        # select highest resolution 
+        stream = yt.streams.get_highest_resolution()
+    
+        # create video file string 
+        file = "video" + str(num) + ".mp4"
+        stream.download(output_path = ".", filename=file)  
+        video_path = file
+    
+    except Exception as e : 
+        print(f"ERROR : {e}")
+        video_path = "" 
         
-    # try : 
-    #     vid = cv2.VideoCapture(url)
-    #     print(f"File Opened: {url}")
+    return video_path
+        
     
-    # except Exception as e : 
-    #     print(f"Error: Could not open video - {e}") 
-    #     return 
     
+
+
+    
+# csv_file_path = "/Users/kendallboesch/Desktop/CS5351-SeniorDesign/TestCQ/omg_TrainVideos.csv"
 csv_file_path = "/Users/kendallboesch/Desktop/CS5351-SeniorDesign/TestCQ/SampleTraining.csv"
-    
+
     # Open & read training dataset csv 
 with open(csv_file_path, 'r') as csvfile :
+    num_vid = 0
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader : 
         url = row['link']
         t_start = float(row['start'])
         t_end = float(row['end'])
-            
+        video = row['video']
+        utterance = row['utterance']
+        arousal = float(row['arousal'])
+        valence = float(row['valence'])
+        emotionMaxVote = int(row['EmotionMaxVote'])
+
             # Process video 
-            
-        process_video(url, t_start, t_end)
+        converted_file_path = download_youtube(url, num_vid)    
+        num_vid += 1
+        process_video(converted_file_path, t_start, t_end, video, utterance, arousal, valence, emotionMaxVote, accessed, processed)
